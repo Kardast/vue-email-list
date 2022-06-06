493b61fadc783661bbf5cae314b4b08b4456443e
Kardast <dottorsf@gmail.com>
2022-06-06 16:47:30 +0200

    10 emails

--- js/script.js
+++ js/script.js
@@ -1,21 +1,17 @@
 const myEmail = new Vue ({
     el: "#mySpace",
     data: {
-        email: [
-            e1 = "",
-            e2 = ""
-        ]
+        email: []
     },
     mounted(){
-        for (let i = 0; i < 2; i++) {
+        for (let i = 0; i < 10; i++) {
             
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
             .then((result) => {
                 // questa costante mi va a prendere la proprietà dell'oggetto dell'API che mi interessa
                 const risultato = result.data.response;
                 console.log("prova", risultato);
-                this.email[i] = risultato;
-
+                this.email.push(risultato);
             });
         }
 
